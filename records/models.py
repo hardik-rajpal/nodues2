cddb4537524c0decfbb7ee58cf539ee83f6bc464
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-05-27 18:58:15 +0530

    updated model, post_reqs, clear_balnace with new models

--- records/models.py
+++ records/models.py
@@ -1,32 +1,48 @@
 from turtle import ondrag
 from django.utils import timezone
 from django.db import models
+
+
 class Department(models.Model):
-    name=models.CharField(max_length=100,blank=False,null=False)
-    email=models.CharField(max_length=200,blank=False,null=False)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    email = models.CharField(max_length=200, blank=False, null=False)
+
     def __str__(self):
         return self.name
+
+
 class Requirement(models.Model):
-    department = models.ForeignKey(Department,max_length=50,on_delete=models.CASCADE)
-    comment = models.TextField(max_length=500,default='None')
+    department = models.ForeignKey(Department,
+                                   max_length=50,
+                                   on_delete=models.CASCADE)
     time_posted = models.DateTimeField(default=timezone.now)
-    roll_number = models.CharField(max_length=11,blank=False,null=False)
+    roll_number = models.CharField(max_length=11, blank=False, null=False)
+
     def __str__(self):
-        return self.department.name+':'+self.roll_number
+        return self.department.name + ':' + self.roll_number
+
+
 class Queries(models.Model):
-    title = models.CharField(max_length=50,blank=True)
+    title = models.CharField(max_length=50, blank=True)
     comment = models.TextField(max_length=500)
-    document_id = models.CharField(max_length=50,blank=True)
-    status_check = models.BooleanField(default=None,null=True)
+    document_id = models.CharField(max_length=50, blank=True)
+    status_check = models.BooleanField(default=None, null=True)
     time_posted = models.DateTimeField()
-    response = models.TextField(max_length=500,default='')
+    response = models.TextField(max_length=500, default='')
     requirement = models.ForeignKey(Requirement, on_delete=models.CASCADE)
+
     def __str__(self):
-        return str(self.requirement)+'->'+self.comment[:min(len(self.comment),20)]
+        return str(self.requirement
+                   ) + '->' + self.comment[:min(len(self.comment), 20)]
+
+
 class Balance(models.Model):
-    requirement = models.ForeignKey(Requirement,on_delete=models.CASCADE)
+    requirement = models.ForeignKey(Requirement, on_delete=models.CASCADE)
     amount = models.IntegerField(default=0)
+    #equivalent to isMonetary or not.
+    index = models.IntegerField(default=1)
     isMonetary = models.BooleanField(default=True)
-    comment = models.TextField(max_length=500,default='None')
+    comment = models.TextField(max_length=500, default='None')
+
     def __str__(self):
-        return str(self.requirement)+self.comment[:20]
+        return str(self.requirement) + self.comment[:20]
--- records/views.py
+++ records/views.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 from accounts.models import UserProfile
 from accounts.models import AdminProfile
 from accounts.helpers import userFromRequestByCookies
+from records.models import Balance
 from records.models import Queries
 from records.serializer import QuerySerializer
 from records.models import Department
@@ -18,65 +19,74 @@ from records.models import Requirement
 from records.serializer import RequirementSerializer
 import hashlib
 
+
 class RequirementViewSet(viewsets.ViewSet):
     authentication_classes = []
     permission_classes = ()
-    def clearBalance(viewset, request:HttpRequest):
+
+    def clearBalance(viewset, request: HttpRequest):
+        #should be called with balance id now.
         userobj = userFromRequestByCookies(request)
         try:
             upro = UserProfile.objects.get(user=userobj)
         except:
-            return Response({'message':'User profile does not exist.'},status=403)
-        upro:UserProfile
-        adminpros = AdminProfile.objects.filter(user=upro);
-        if(adminpros.__len__()==0):
-            return Response({'message':'Admin Profile does not exist.'},status=403)
-        reqID = request.GET.get('reqID')
-        req=None
+            return Response({'message': 'User profile does not exist.'},
+                            status=403)
+        upro: UserProfile
+        adminpros = AdminProfile.objects.filter(user=upro)
+        if (adminpros.__len__() == 0):
+            return Response({'message': 'Admin Profile does not exist.'},
+                            status=403)
+        balanceID = request.GET.get('reqID')
+        balance = None
         try:
-            req = Requirement.objects.get(id=reqID)
+            balance = Balance.objects.get(id=balanceID)
         except:
-            return Response({'detail':'Requirement with given id not found.'})
-        req:Requirement
-        req.balance=0;
+            return Response({'detail': 'Requirement with given id not found.'})
+        balance: Balance
+        balance.amount = 0
         t_temp = timezone.now()
-        req.comment+=f"; Cleared at {' '.join(t_temp.isoformat().split('.')[0].split('T'))} by {upro.roll_no}"
-        req.save()
-        return Response({},status=200)
-    def get_requirements(viewset,request:HttpRequest):
+        balance.comment += f"; Cleared at {' '.join(t_temp.isoformat().split('.')[0].split('T'))} by {upro.roll_no}"
+        balance.save()
+        return Response({}, status=200)
+
+    def get_requirements(viewset, request: HttpRequest):
         """Get requirements."""
-        print(request.user)
         userobj = userFromRequestByCookies(request)
         try:
-            queryset = UserProfileFullSerializer.setup_eager_loading(UserProfile.objects)
+            queryset = UserProfileFullSerializer.setup_eager_loading(
+                UserProfile.objects)
             user_profile = queryset.get(user=userobj)
         except UserProfile.DoesNotExist:
-            return Response({'message': "UserProfile doesn't exist"}, status=500)
+            return Response({'message': "UserProfile doesn't exist"},
+                            status=500)
         # print('hi')
         roll_no = user_profile.roll_no
         adminAccountList = AdminProfile.objects.filter(user=user_profile)
-        
+
         user_profile.last_ping = timezone.now()
         user_profile.save(update_fields=['last_ping'])
-        
+
         reqs = None
-        if(len(adminAccountList)>0):
-            reqs = Requirement.objects.filter(department=adminAccountList[0].department)    
+        if (len(adminAccountList) > 0):
+            reqs = Requirement.objects.filter(
+                department=adminAccountList[0].department)
         else:
             reqs = Requirement.objects.filter(roll_number=roll_no)
         print('hi1')
-        if(reqs==None):
-            return {'data':[],'error':'Neither admin nor recorded student'}
-        
-        data = RequirementSerializer(reqs,many=True).data
-        resp = Response({'data':data},headers={'Access-Control-Allow-Origin':True})
+        if (reqs == None):
+            return {'data': [], 'error': 'Neither admin nor recorded student'}
+
+        data = RequirementSerializer(reqs, many=True).data
+        resp = Response({'data': data},
+                        headers={'Access-Control-Allow-Origin': True})
         print(resp.headers)
         return resp
 
-    def parse_data(datastr:str):
-        datastr = datastr.replace('\r','')
+    def parse_data(datastr: str):
+        datastr = datastr.replace('\r', '')
         lines = datastr.split('\n')
-        lines = list(filter(lambda x:len(x)!=0,lines))
+        lines = list(filter(lambda x: len(x) != 0, lines))
         lines = [line.split(',') for line in lines]
         data = {}
         try:
@@ -87,24 +97,32 @@ class RequirementViewSet(viewsets.ViewSet):
             lines.pop(0)
         for line in lines:
             data[line[0]] = {
-                'balance':int(line[1]) ,
-                'comment':line[2]
-                }
-
+                'index': int(line[1]),
+                'amount': int(line[2]),
+                'isMonetary': line[3],
+                'comment': line[4]
+            }
         return data
-    def updateRecords(data:dict,department:Department):
-        print(data)
+
+    def updateRecords(data: dict, department: Department):
+        # print(data)
         # return
+        # balance = Requirement.objects.get_or_create(roll_)
         for key in data.keys():
-            req,_ = Requirement.objects.get_or_create(roll_number=key,department=department)
-            req:Requirement
-            req.balance = data[key]['balance']
-            req.comment = data[key]['comment']
-            req.time_posted = timezone.now()
-            print(req.time_posted)
-            print(req)
-            req.save()
-    def post_requirements(viewset,request):
+            balance, _ = Requirement.objects.get_or_create(
+                roll_number=key, department=department)
+            balance: Requirement
+            balance.time_posted = timezone.now()
+            balance = Balance.objects.get_or_create(requirement=balance,
+                                                    index=data[key]['index'])
+            balance: Balance
+            balance.amount = data[key]['amount']
+            balance.isMonetary = data[key]['isMonetary'] == '1'
+            balance.comment = data[key]['comment']
+            balance.save()
+            balance.save()
+
+    def post_requirements(viewset, request):
         # UploadFileForm(request.POST)
         # print()
         # print()
@@ -118,105 +136,133 @@ class RequirementViewSet(viewsets.ViewSet):
             admin = AdminProfile.objects.get(user=userpro)
             department = admin.department
         except Exception as e:
-            return Response({'error':str(e)+f' UserID: {userid}'},status=403)
+            return Response({'error': str(e) + f' UserID: {userid}'},
+                            status=403)
         data = request.FILES['file'].read().decode('utf-8')
         parsedData = RequirementViewSet.parse_data(data)
-        RequirementViewSet.updateRecords(parsedData,department)
-        return Response({'status':200})
+        RequirementViewSet.updateRecords(parsedData, department)
+        return Response({'status': 200})
+
 
 class QueriesViewSet(viewsets.ViewSet):
     authentication_classes = []
     permission_classes = ()
-    def getQueries(viewset,request:HttpRequest):
+
+    def getQueries(viewset, request: HttpRequest):
         logs = ''
         userobj = userFromRequestByCookies(request)
         user_profile = UserProfile.objects.get(user=userobj)
         roll_no = user_profile.roll_no
-        responded = request.GET.get('responded')=='1'
-        pagenum=request.GET.get('page')
+        responded = request.GET.get('responded') == '1'
+        pagenum = request.GET.get('page')
         adminAccountList = AdminProfile.objects.filter(user=user_profile)
         queries = None
         reqs = None
-        if(len(adminAccountList)==0):
+        if (len(adminAccountList) == 0):
             reqs = Requirement.objects.filter(roll_number=roll_no)
         else:
-            reqs = Requirement.objects.filter(department=adminAccountList[0].department)
+            reqs = Requirement.objects.filter(
+                department=adminAccountList[0].department)
             # queries = Queries.objects.filter(requirement__in=reqs)
         if not responded:
-            queries=Queries.objects.filter(requirement__in=reqs,status_check=None)
+            queries = Queries.objects.filter(requirement__in=reqs,
+                                             status_check=None)
         else:
-            queries = Queries.objects.filter(requirement__in=reqs,status_check__in=[True,False])
+            queries = Queries.objects.filter(requirement__in=reqs,
+                                             status_check__in=[True, False])
 
-        queries = QuerySerializer(queries,many=True).data
+        queries = QuerySerializer(queries, many=True).data
         paginator = Paginator(queries, 2)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(pagenum)
         return Response({
-            'rn':roll_no,
-            'rir':responded,
-            'rawqs':queries,
-            'reqs':RequirementSerializer(reqs,many=True).data,
-            'data':page_obj.object_list,
-            'count':paginator.num_pages,
-            'next':page_obj.next_page_number() if page_obj.has_next() else None,
-            'prev':page_obj.previous_page_number() if page_obj.has_previous() else None})
-    def upload_file_queries(viewset,request):
+            'rn':
+            roll_no,
+            'rir':
+            responded,
+            'rawqs':
+            queries,
+            'reqs':
+            RequirementSerializer(reqs, many=True).data,
+            'data':
+            page_obj.object_list,
+            'count':
+            paginator.num_pages,
+            'next':
+            page_obj.next_page_number() if page_obj.has_next() else None,
+            'prev':
+            page_obj.previous_page_number()
+            if page_obj.has_previous() else None
+        })
+
+    def upload_file_queries(viewset, request):
         try:
             file_received = request.FILES['file']
-            ext = file_received.name.split('.')[-1]    
-            def handle_uploaded_file(f,ext):
+            ext = file_received.name.split('.')[-1]
+
+            def handle_uploaded_file(f, ext):
                 md5_hash = hashlib.md5()
                 # Read and update hash in chunks of 4K
-                for byte_block in iter(lambda: f.read(4096),b""):
+                for byte_block in iter(lambda: f.read(4096), b""):
                     md5_hash.update(byte_block)
                 # print(md5_hash.hexdigest())
                 hash_val = md5_hash.hexdigest()
                 import os
                 print(os.getcwd())
-                destination = open('./records/storage/' + hash_val + '.' + ext, 'bw+')
+                destination = open('./records/storage/' + hash_val + '.' + ext,
+                                   'bw+')
                 for chunk in f.chunks():
                     destination.write(chunk)
                 return hash_val
-            MD5_hash = handle_uploaded_file(file_received,ext)
-            return Response({'docID':MD5_hash + '.' + ext})
+
+            MD5_hash = handle_uploaded_file(file_received, ext)
+            return Response({'docID': MD5_hash + '.' + ext})
         except:
             return Response({'error': 'failed to upload file'})
-    def get_uploaded_file(viewset,request:HttpRequest, pk):
+
+    def get_uploaded_file(viewset, request: HttpRequest, pk):
         import os
-        if os.path.exists('./records/storage/'+pk):
-            file = open('./records/storage/'+pk, 'rb')
+        if os.path.exists('./records/storage/' + pk):
+            file = open('./records/storage/' + pk, 'rb')
             return FileResponse(file)
         else:
             return Response({'message': 'file not found'})
-    def postQueryByStudent(viewset,request):
+
+    def postQueryByStudent(viewset, request):
         data = json.loads(request.body.decode())
         #TODO:Check auth permissions
-        req = None
+        balance = None
         print(data)
         try:
-            req = Requirement.objects.get(id=data['reqID'])
+            balance = Requirement.objects.get(id=data['reqID'])
         except:
-            return Response({'error':'Requirement not found'},500)
-        Queries.objects.create(requirement=req,comment=data['comment'],document_id=data['docID'],time_posted=timezone.now())
-        return Response({},200)
-    def respondToQuery(viewset,request):
+            return Response({'error': 'Requirement not found'}, 500)
+        Queries.objects.create(requirement=balance,
+                               comment=data['comment'],
+                               document_id=data['docID'],
+                               time_posted=timezone.now())
+        return Response({}, 200)
+
+    def respondToQuery(viewset, request):
         data = json.loads(request.body.decode())
         data['response']
         data['queryID']
         print(data)
-        userobj= userFromRequestByCookies(request)
+        userobj = userFromRequestByCookies(request)
         roll_no = UserProfile.objects.get(user=userobj).roll_no
         try:
-            queryset = UserProfileFullSerializer.setup_eager_loading(UserProfile.objects)
+            queryset = UserProfileFullSerializer.setup_eager_loading(
+                UserProfile.objects)
             user_profile = queryset.get(roll_no=roll_no)
         except UserProfile.DoesNotExist:
-            return Response({'message': "UserProfile doesn't exist"}, status=500)
+            return Response({'message': "UserProfile doesn't exist"},
+                            status=500)
         roll_no = user_profile.roll_no
         adminAccountList = AdminProfile.objects.filter(user=user_profile)
-        if(len(adminAccountList)==0):
-            return Response({'error':'No admin account found'},status=401)
+        if (len(adminAccountList) == 0):
+            return Response({'error': 'No admin account found'}, status=401)
         q = Queries.objects.get(id=data['queryID'])
         q.response = data['response']
         q.status_check = data['accepted']
         q.save()
-        return Response({},status=200)
+        return Response({}, status=200)
